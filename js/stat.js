1e5976bff98a616a7e9d58ac574f8791c7eb2439
quadabra <gr.maks@gmail.com>
2017-11-20 02:14:02 +0300

    2nd task fix

--- js/stat.js
+++ js/stat.js
@@ -2,30 +2,30 @@
 
 window.renderStatistics = function (ctx, names, times) {
 
-  var cloudX = [100, 170, 240, 350, 420, 520, 530, 520, 320, 220, 100, 110, 100];
-  var cloudY = [10, 0, 15, 5, 0, 10, 150, 270, 280, 275, 260, 160, 10];
+  var cloudX = [120, 120, 140, 140, 480, 480, 500, 500, 520, 520, 515, 515, 520, 520, 500, 500, 480, 480, 140, 140, 120, 120, 100, 100, 105, 105, 100, 100];
+  var cloudY = [30, 10, 10, 15, 15, 10, 10, 30, 30, 40, 40, 250, 250, 260, 260, 280, 280, 275, 275, 280, 280, 260, 260, 250, 250, 40, 40, 30];
 
   var polygonDraw = function (shiftX, shiftY) {
     ctx.beginPath();
-    ctx.moveTo(cloudX + shiftX, cloudY + shiftY);
-    for (var i = 0; i < cloudX.length; i++) {
+    ctx.moveTo(cloudX[0] + shiftX, cloudY[0] + shiftY);
+    for (var i = 1; i < cloudX.length; i++) {
       ctx.lineTo(cloudX[i] + shiftX, cloudY[i] + shiftY);
     }
     ctx.closePath();
     ctx.fill();
   };
 
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';  //тень
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   polygonDraw(10, 10);
   ctx.fillStyle = 'rgba(255, 255, 255, 1.0)';
   polygonDraw(0, 0);
-  ctx.fillStyle = '#000'; // black;
-  ctx.font = '14px PT Mono';
-
-  ctx.fillText('Ура вы победили!', 120, 40);
+  ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
 
   var max = -1;
   var maxIndex = -1;
+  var min = times[0];
+  var minIndex = 0;
 
   for (var i = 0; i < times.length; i++) {
     var time = times[i];
@@ -33,27 +33,37 @@ window.renderStatistics = function (ctx, names, times) {
       max = time;
       maxIndex = i;
     }
+    if (time <= min) {
+      min = time;
+      minIndex = i;
+    }
   }
 
-  var histogramWidth = 150;              // px;
-  var step = histogramWidth / max; // px;
+  if (names[minIndex] === 'Вы') {
+    ctx.fillText('Ура вы победили!', 120, 40);
+  } else {
+    ctx.fillText('Cписок результатов', 120, 40);
+  }
+
+  var histogramWidth = 150;
+  var step = histogramWidth / max;
 
   ctx.fillText('Худшее время: ' + max.toFixed(0) + 'мс у игрока ' + names[maxIndex], 120, 60);
 
-  var barWidth = 40; // px;
-  var indent = 50;    // px;
-  var initialX = 220; // px;
-  var initialY = 240;  // px;
-  var lineHeight = 25;// px;
+  var barWidth = 40;
+  var indent = 90;
+  var initialX = 155;
+  var initialY = 230;
+  var lineHeight = 25;
 
-  for (var i = 0; i < times.length; i++) {
-    if (names[i] === 'Вы') {
+  for (var j = 0; j < times.length; j++) {
+    if (names[j] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1.0)';
     } else {
       ctx.fillStyle = 'rgba(0, 0, 255, ' + Math.ceil(Math.random() * 10) / 10 + ')';
     }
-    ;
-    ctx.fillRect(initialX + indent * i, initialY, barWidth, -times[i] * step);
-    ctx.fillText(names[i], initialX + indent * i, initialY + lineHeight);
+
+    ctx.fillRect(initialX + indent * j, initialY, barWidth, -times[j] * step);
+    ctx.fillText(names[j], initialX + indent * j, initialY + lineHeight);
   }
 };
